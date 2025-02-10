bdcfb9fe40673e22d7afdbc18a883851865a18f4
JakubKolinger <jakub.kolinger.024@pslib.cz>
2025-02-10 10:24:39 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
-radio.setGroup(77)
-let start: boolean = false
+radio.setGroup(999)
+let start: boolean
+start = false
+let timer: boolean
+timer = false
 Sensors.SetLightLevel()
 
 Sensors.OnLightDrop(function() {
@@ -10,8 +13,18 @@ Sensors.OnLightDrop(function() {
 })
 
 input.onButtonPressed(Button.AB, function() {
-     radio.sendNumber(0)
+    radio.sendNumber(0)
 })
+
+radio.onReceivedNumber(function(receivedNumber: number) {
+    
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber(index)
+    }
+
+    
+})
+
 
 basic.forever(function() {
 
